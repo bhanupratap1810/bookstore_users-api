7662ee8007b070f494afad0269d06a983d201071
bhanupratap1810 <bhanupratapbd1@gmail.com>
2021-01-12 01:37:16 +0530

    mysql row insertion

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,7 +2,15 @@ package users
 
 import (
 	"fmt"
+	"github.com/bhanupratap1810/bookstore_users-api/datasources/mysql/users_db"
+	"github.com/bhanupratap1810/bookstore_users-api/utils/date_utils"
 	"github.com/bhanupratap1810/bookstore_users-api/utils/errors"
+	"strings"
+)
+
+const(
+	indexUniqueEmail="email_UNIQUE"
+	queryInsertUser="INSERT INTO users(first_name, last_name, email, date_created) VALUES(?,?,?,?);"
 )
 
 var  (
@@ -10,6 +18,9 @@ var  (
 )
 
 func (user *User) Get() *errors.RestErr {
+	if err:=users_db.Client.Ping(); err!=nil{
+		panic(err)
+	}
 	result:=usersDB[user.Id]
 	if result==nil{
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
@@ -23,13 +34,31 @@ func (user *User) Get() *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr{
-	current:=usersDB[user.Id]
-	if current!=nil {
-		if current.Email==user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	stmt, err:= users_db.Client.Prepare(queryInsertUser)
+	if err!= nil {
+		return errors.NewInternalServerError(err.Error())
 	}
-	usersDB[user.Id]=user
+	defer stmt.Close()
+
+	user.DateCreated=date_utils.GetNowString()
+
+	insertResult,err:=stmt.Exec(user.FirstName,user.LastName,user.Email,user.DateCreated)
+	if err!= nil{
+		if strings.Contains(err.Error(),indexUniqueEmail){
+			return errors.NewBadRequestError(
+				fmt.Sprintf("email %s already exists", user.Email),
+			)
+		}
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to save user:%s", err.Error() ),
+		)
+	}
+	userId,err:=insertResult.LastInsertId()
+	if err!=nil{
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to save user:%s", err.Error()),
+		)
+	}
+	user.Id=userId
 	return nil
 }
